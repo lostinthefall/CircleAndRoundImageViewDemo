3fd7d9ca88be9e0e7f8c16232765e2743bb4cfc6
lostinthefall <zhuangyinwu@gmail.com>
2016-03-30 12:48:52 +0800

    add sliding menu fixed type

--- app/src/main/java/com/atozmak/circleandroundimageviewdemo/widgets/SlidingMenu.java
+++ app/src/main/java/com/atozmak/circleandroundimageviewdemo/widgets/SlidingMenu.java
@@ -1,14 +1,19 @@
 package com.atozmak.circleandroundimageviewdemo.widgets;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+
+import com.atozmak.circleandroundimageviewdemo.R;
 
 /**
  * Created by Mak on 2016/3/29.
@@ -27,22 +32,63 @@ public class SlidingMenu extends HorizontalScrollView {
 
     private boolean once = false;
 
+    private boolean isOpen = false;
+
+    private Button btnSwitch;
+
+    //-----------------------------------------------------
 
     public SlidingMenu(Context context) {
         this(context, null);
     }
 
+    //未设置自定义属性时，会调用此构造方法。
     public SlidingMenu(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
+
+    }
+
+    //当使用了自定义属性时，调用此构造方法。
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
         mScreenWidth = getResources().getDisplayMetrics().widthPixels;
 
-        //把dp转为px
+/*        //把dp转为px
         mMenuRightPadding = (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
-                50,
+                100,
                 context.getResources().getDisplayMetrics()
-        );
+        );*/
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SlidingMenu, defStyleAttr, 0);
+
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.SlidingMenu_rightPadding:
+                    mMenuRightPadding = a.getDimensionPixelSize(
+                            attr,
+                            (int) TypedValue.applyDimension(
+                                    TypedValue.COMPLEX_UNIT_DIP,
+                                    100,
+                                    context.getResources().getDisplayMetrics()
+                            )
+                    );
+                    break;
+            }
+        }
+
+
+        a.recycle();
+
     }
+
+    //-----------------------------------------------------
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -67,10 +113,11 @@ public class SlidingMenu extends HorizontalScrollView {
         super.onLayout(changed, l, t, r, b);
 
         if (changed) {
+            //scrollTo移动的是该view的内容，
+            // this在这里是自定义的horizontalScrollView，
+            //即在onLayout的时候就把mMenu隐藏了。
             this.scrollTo(mMenuWidth, 0);
         }
-
-
     }
 
     @Override
@@ -79,17 +126,84 @@ public class SlidingMenu extends HorizontalScrollView {
         int action = ev.getAction();
         switch (action) {
             case MotionEvent.ACTION_UP:
+                //  getScrollX()  ：
+                // The offset, in pixels, by which the content of this view is scrolled horizontally.
                 int scrollX = getScrollX();
 
+                // menu的左上角为（0，0），
+                // getScrollX()是整个horizontal已经移动的距离，大小为menu的宽度，
+                // 当用户手指开始滑动，getScrollX()获得新得值，
+                // 如果getScrollX()比mMenuWidth/2要大，
+                // 就是说用户在content界面向右滑动的距离小于mMenuWidth/2，
+                // 那么显示content，把menu移回去。
                 if (scrollX >= mMenuWidth / 2) {
+                    //mMenuWidth为正，view的内容左移。
                     this.smoothScrollTo(mMenuWidth, 0);
+                    isOpen = false;
                 } else {
                     this.smoothScrollTo(0, 0);
+                    isOpen = true;
                 }
                 return true;
         }
-
-
         return super.onTouchEvent(ev);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        btnSwitch = (Button) findViewById(R.id.idBtn);
+        btnSwitch.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle();
+            }
+        });
+    }
+
+    /**
+     * 打开菜单
+     */
+    public void openMenu() {
+        if (isOpen) return;
+        this.smoothScrollTo(0, 0);
+        isOpen = true;
+    }
+
+    /**
+     * 关闭菜单
+     */
+    public void closeMenu() {
+        if (!isOpen) return;
+        this.smoothScrollTo(mMenuWidth, 0);
+        isOpen = false;
+    }
+
+    /**
+     * 切换菜单
+     */
+    public void toggle() {
+        if (isOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+
+    //添加了之后以下代码之后会使得menu有固定的效果。
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+
+        //变化 ：1 --> 0
+        float scale = l * 1.0f / mMenuWidth;
+        // menu这个view是有自己的位置的，就是horizontalView中连着contentView的左边，
+        // 按照规定，如果滑动contentView，menu也就会连着被滑出来，
+        // 现在想要固定menu的话，就要使得在用户不断滑动的过程中，让menu都移到一个确定的位置，
+        // 从视觉效果上来看的话，menu就固定着不动了。
+        // 在用户想要滑出menu的一瞬间，menu已经向右移动了mMenuWidth*1的距离。
+        // 用户几乎滑动了menu宽度这么长的距离的一瞬间，menu已经向右移动了mMenuWidth*0的距离。
+        mMenu.setTranslationX(mMenuWidth * scale);
+
     }
 }
